6f48da12a885d670eb76db965afa24f4299d31ed
Alejandro Robles <alejandro_r1704@hotmail.com>
2020-01-03 08:31:07 -0600

    Project created

---
+++ PushNotification/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PushNotification
+//
+//  Created by Omar Alejandro Robles Altamirano on 03/01/20.
+//  Copyright © 2020 Alejandro Robles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PushNotification
 This project is taken from Harendra Tiwari Youtube channel and re-written by me
+
+https://www.youtube.com/watch?v=Oyi7QXqETW4&feature=youtu.be
